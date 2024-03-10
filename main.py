38fa2ff8c3156cdb655acdd9bfcdd551b739449f
AlexandrLobachev <alexandrlobachev@gmail.com>
2024-03-10 23:13:43 +0300

    Добавлены зависимости

--- main.py
+++ main.py
@@ -2,79 +2,58 @@ import copy
 import psycopg2
 
 
+CREDS = {
+    'dbname': 'test_task_doc_db',
+    'host': 'localhost',
+    'user': 'db_user',
+    'password': 'password111',
+}
+
+
 def get_document() -> tuple:
     """Возвращает не обработанный документ."""
     try:
-        connection = psycopg2.connect(
-            dbname='test_task_doc_db',
-            host='localhost',
-            user='db_user',
-            password='password111',
-        )
-        cursor = connection.cursor()
-
-        query = '''
-        SELECT * FROM public.documents
-        WHERE document_type = 'transfer_document'
-        AND processed_at IS NULL
-        ORDER BY recieved_at ASC
-        '''
-        cursor.execute(query)
-        return cursor.fetchone()
-    except (Exception, psycopg2.Error) as error:
-        print("Ошибка при работе с PostgreSQL", error)
+        conn = psycopg2.connect(**CREDS)
+        with conn.cursor() as curs:
+            curs.execute(
+                '''
+                SELECT * FROM public.documents
+                WHERE document_type = 'transfer_document'
+                AND processed_at IS NULL
+                ORDER BY recieved_at ASC
+                ''')
+            return curs.fetchone()
     finally:
-        if connection:
-            cursor.close()
-            connection.close()
+        curs.close()
+        conn.close()
 
 
 def get_data(objects: tuple) -> list:
     """Возваращает объекты data c parent из списка document_data.objects."""
     try:
-        connection = psycopg2.connect(
-            dbname='test_task_doc_db',
-            host='localhost',
-            user='db_user',
-            password='password111',
-        )
-        cursor = connection.cursor()
-
-        query = '''
-        SELECT * FROM public.data
-        WHERE data.parent IN %s
-        '''
-        cursor.execute(query, (objects,))
-        data = cursor.fetchall()
-        return data
-    except (Exception, psycopg2.Error) as error:
-        print("Ошибка при работе с PostgreSQL", error)
+        conn = psycopg2.connect(**CREDS)
+        with conn.cursor() as curs:
+            curs.execute(
+                '''
+                SELECT * FROM public.data
+                WHERE data.parent IN %s
+                ''', (objects,))
+            return curs.fetchall()
     finally:
-        if connection:
-            cursor.close()
-            connection.close()
+        curs.close()
+        conn.close()
 
 
 def update_data(query: str, values: dict):
     """Обновляет данные в БД."""
     try:
-        connection = psycopg2.connect(
-            dbname='test_task_doc_db',
-            host='localhost',
-            user='db_user',
-            password='password111',
-        )
-        cursor = connection.cursor()
-
-        cursor.executemany(query, (values,))
-        connection.commit()
-
-    except (Exception, psycopg2.Error) as error:
-        print("Ошибка при работе с PostgreSQL", error)
+        conn = psycopg2.connect(**CREDS)
+        with conn.cursor() as curs:
+            curs.executemany(query, (values,))
+            conn.commit()
     finally:
-        if connection:
-            cursor.close()
-            connection.close()
+        curs.close()
+        conn.close()
 
 
 def parce_document(document: tuple) -> tuple:
@@ -108,13 +87,14 @@ def check_data(data: list, values: dict) -> tuple:
 def get_query(new_param: dict = None) -> str:
     """Формирует запрос к БД, для изменения параметров объектов."""
     query_for_doc = '''
-     UPDATE public.documents 
-     SET processed_at = now() 
+     UPDATE public.documents
+     SET processed_at = now()
      WHERE documents.doc_id = %(document)s;
      '''
 
     if not new_param:
         return query_for_doc
+
     def get_parametrs(new_param: dict) -> str:
         values = []
         for field, value in new_param.items():
@@ -124,8 +104,8 @@ def get_query(new_param: dict = None) -> str:
 
     parametrs = get_parametrs(new_param)
     query_for_data = f'''
-    UPDATE public.data 
-    SET {parametrs} 
+    UPDATE public.data
+    SET {parametrs}
     WHERE data.object IN %(objects)s;'''
     return ' '.join((query_for_data, query_for_doc))
 
@@ -141,6 +121,9 @@ def main():
     """Основная логика скрипта."""
     try:
         document = get_document()
+        if not document:
+            print('Нет документа!')
+            return False
         doc_id, objects, new_values, old_values = parce_document(document)
         data = get_data(objects)
         objects_for_change = check_data(data, old_values)
@@ -150,8 +133,10 @@ def main():
             query = get_query(new_values)
         values_for_query = get_values(new_values, objects_for_change, doc_id)
         update_data(query, values_for_query)
+        print('Документ обработан.')
         return True
-    except:
+    except (Exception, psycopg2.Error) as error:
+        print('Ошибка при работе скрипта:', error)
         return False
 
 
